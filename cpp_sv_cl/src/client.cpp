24af90b3755b9ecd0a53ace04bd880e4994b2d53
anhHa <hoangha5657@gmail.com>
2024-07-23 21:30:33 +0700

    add more server

--- cpp_sv_cl/src/client.cpp
+++ cpp_sv_cl/src/client.cpp
@@ -1,5 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <example_interfaces/srv/add_two_ints.hpp>
+#include "example_interfaces/srv/trigger.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
 
 #include <chrono>
 #include <cstdlib>
@@ -7,43 +9,102 @@
 
 using namespace std::chrono_literals;
 
+
 int main(int argc, char **argv) 
 {
     rclcpp::init(argc, argv);
     
-    if(argc != 3){
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "usage: add_two_ints_client X Y");
-        return 1;
-    }
+    if(argc == 1) {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "send to trigger server");
+        std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("trigger_client");
+        rclcpp::Client<example_interfaces::srv::Trigger>::SharedPtr client = 
+        node->create_client<example_interfaces::srv::Trigger>("trigger");
 
-    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
-    rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client = 
-    node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+        auto request = std::make_shared<example_interfaces::srv::Trigger::Request>();
 
-    auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
-    request->a = atoll(argv[1]);
-    request->b = atoll(argv[2]);
-    
-    while (!client->wait_for_service(1s))
-    {
-        if(!rclcpp::ok()) 
+        while (!client->wait_for_service(1s))
+          {
+              if(!rclcpp::ok()) 
+              {
+                  RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupt while waiting for service");
+                  return 0;
+              }
+              RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+
+          }
+          auto result = client->async_send_request(request);
+
+            if (rclcpp::spin_until_future_complete(node, result) ==
+          rclcpp::FutureReturnCode::SUCCESS)
         {
-            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupt while waiting for service");
-            return 0;
+          RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "trigger %s", result.get()->message);
+        } else {
+          RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service trigger");
         }
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting agin...");
+    }
+    else if(argc == 3){
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "send to add two ints server");
+      std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_client");
+      rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client = 
+      node->create_client<example_interfaces::srv::AddTwoInts>("add_two_ints");
+
+      auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+      request->a = atoll(argv[1]);
+      request->b = atoll(argv[2]);
+
+      while (!client->wait_for_service(1s))
+        {
+            if(!rclcpp::ok()) 
+            {
+                RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupt while waiting for service");
+                return 0;
+            }
+            RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting agin...");
+
+        }
+        auto result = client->async_send_request(request);
+
+          if (rclcpp::spin_until_future_complete(node, result) ==
+        rclcpp::FutureReturnCode::SUCCESS)
+      {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
+      } else {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+      }
 
     }
-    auto result = client->async_send_request(request);
 
-      if (rclcpp::spin_until_future_complete(node, result) ==
-    rclcpp::FutureReturnCode::SUCCESS)
-  {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %ld", result.get()->sum);
-  } else {
-    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
-  }
+    else if(argc == 2){
+      std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("set_client");
+      rclcpp::Client<example_interfaces::srv::SetBool>::SharedPtr client = 
+      node->create_client<example_interfaces::srv::SetBool>("set");
 
+      auto request = std::make_shared<example_interfaces::srv::SetBool::Request>();
+      request->data = (bool)(argv[1]);
+
+      while (!client->wait_for_service(1s))
+      {
+          if(!rclcpp::ok()) 
+          {
+              RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupt while waiting for service");
+              return 0;
+          }
+          RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting agin...");
+
+      }
+      auto result = client->async_send_request(request);
+
+        if (rclcpp::spin_until_future_complete(node, result) ==
+      rclcpp::FutureReturnCode::SUCCESS)
+    {
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "set bool: %s", result.get()->message);
+    } else {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service set bool");
+    }
+      }
+    else 
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "No server match your argument list");
+      
   rclcpp::shutdown();
   return 0;
 }
--- cpp_sv_cl/src/server.cpp
+++ cpp_sv_cl/src/server.cpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/srv/add_two_ints.hpp"
-
+#include "example_interfaces/srv/trigger.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
 #include <memory>
 
 void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
@@ -12,17 +13,43 @@ void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> req
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
 }
 
+void trig(const std::shared_ptr<example_interfaces::srv::Trigger::Request> request,
+          std::shared_ptr<example_interfaces::srv::Trigger::Response>      response)
+{
+  response->success = 1;
+  response->message = "Successful trigged the service\n";
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\n");
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%d], [%s]\n", (bool)response->success, response->message);
+}
+
+void set(const std::shared_ptr<example_interfaces::srv::SetBool::Request> request,
+          std::shared_ptr<example_interfaces::srv::SetBool::Response>      response)
+{
+  response->success = request->data;
+  response->message = "Successfull set bool variable of superman to";
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request %d\n", request->data);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: %s %d\n", response->message, (bool)response->success);
+}
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
+  std::shared_ptr<rclcpp::Node> node_add = rclcpp::Node::make_shared("add_two_ints_server");
+  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service_add =
+    node_add->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
 
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
-    node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
+  std::shared_ptr<rclcpp::Node> node_trig = rclcpp::Node::make_shared("trigger_server");
+  rclcpp::Service<example_interfaces::srv::Trigger>::SharedPtr service_trig =
+    node_trig->create_service<example_interfaces::srv::Trigger>("trigger", &trig);
 
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
+  std::shared_ptr<rclcpp::Node> node_set = rclcpp::Node::make_shared("set_server");
+  rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr service_set =
+    node_set->create_service<example_interfaces::srv::SetBool>("set", &set);
 
-  rclcpp::spin(node);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready all three.");
+  rclcpp::spin(node_set);
+  rclcpp::spin(node_add);
+//  TODO loi la do 3 dong nay chi chay 1 dong dau tien
+  rclcpp::spin(node_trig);
   rclcpp::shutdown();
 }
